b11a4fd95e6294d14607efa391b4add836c177a2
Sai deekshith <orugantideekshith2006@gmail.com>
2026-03-09 19:48:57 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -7,17 +7,22 @@ use_upper = input("Include Uppercase? (y/n): ").lower() == 'y'
 use_lower = input("Include Lowercase? (y/n): ").lower() == 'y'
 use_digits = input("Include Digits? (y/n): ").lower() == 'y'
 use_symbols = input("Include Symbols? (y/n): ").lower() == 'y'
+
 if length < 4:
     print("Password length must be at least 4 characters.")
     exit()
+
 characters = ""
 
 if use_upper:
     characters += string.ascii_uppercase
+
 if use_lower:
     characters += string.ascii_lowercase
+
 if use_digits:
     characters += string.digits
+
 if use_symbols:
     characters += string.punctuation
 
